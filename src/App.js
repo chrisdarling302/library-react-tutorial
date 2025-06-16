ae7c18f64626e7fc971493c77b9face9191375f0
Chris <cldarling@gmail.com>
2025-06-16 16:05:33 -0400

    nav complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Nav from "./components/Nav";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars, faShoppingCart, faTimes } from "@fortawesome/free-solid-svg-icons";
 
+library.add(faBars, faShoppingCart, faTimes)
 
 function App() {
   return (
     <div className="App">
-      test
+      <Nav />
     </div>
   );
 }
